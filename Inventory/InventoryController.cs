434c41cfa98be400eaec6014b8dfb9da071f6c01
lumos <206481784+lumos-gna@users.noreply.github.com>
2025-04-23 10:39:46 +0900

    feat : inventory 백업추가

---
+++ Inventory/InventoryController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprtaaaaDungeon
+{
+    public class InventoryController
+    {
+        private List<InventoryItemData> items;
+        private Dictionary<GameEnum.ItemType, List<InventoryItemData>> typeItemDict;
+
+        public List<InventoryItemData> Items => items;
+        public Dictionary<GameEnum.ItemType, List<InventoryItemData>> CategorizedItems => typeItemDict;
+
+        public InventoryController()
+        {
+            items = GameManager.Instance.InventoryItems;
+
+            typeItemDict = new Dictionary<GameEnum.ItemType, List<InventoryItemData>>();
+
+            foreach (InventoryItemData item in items)
+            {
+                GameEnum.ItemType type = item.ItemData.Type;
+
+                if (typeItemDict.ContainsKey(type))
+                {
+                    typeItemDict[type].Add(item);
+                }
+                else
+                {
+                    typeItemDict[type] = new List<InventoryItemData> { item };
+                }
+            }
+        }
+
+        public void PrintItems()
+        {
+            foreach (KeyValuePair<GameEnum.ItemType, List<InventoryItemData>> pair in typeItemDict)
+            {
+                Console.WriteLine("[" + pair.Key + "]");
+                foreach (InventoryItemData item in pair.Value)
+                {
+                    string prefix = item.IsEquip ? "[E] " : "   ";
+                    string effect = item.ItemData.Type switch
+                    {
+                        GameEnum.ItemType.Weapon => "공격력 +" + item.ItemData.StatData.Attack,
+                        GameEnum.ItemType.Armor => "방어력 +" + item.ItemData.StatData.Defense,
+                        _ => ""
+                    };
+                    Console.WriteLine("- " + prefix + item.ItemData.Name + " | " + effect);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void PrintItemNames()
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                InventoryItemData item = items[i];
+                string prefix = item.IsEquip ? "[E] " : "   ";
+                Console.WriteLine((i + 1).ToString() + ". " + prefix + item.ItemData.Name);
+            }
+        }
+    }
+}
